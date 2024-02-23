b3b7023484c84cbb5d5c78c9ece059b225d1c6f5
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-23 16:19:43 +0100

    Splitted nav into smaller components

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
+import Search from "./search";
+import Logo from "./logo";
+import Results from "./results";
 
 function Navbar() {
-    const [query, setQuery] = useState("");
     return (
         <nav className="nav-bar">
-            <div className="logo">
-                <span role="img">🍿</span>
-                <h1>usePopcorn</h1>
-            </div>
-            <input
-                className="search"
-                type="text"
-                placeholder="Search movies..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <p className="num-results">
-                Found <strong>X</strong> results
-            </p>
+            <Logo />
+            <Search />
+            <Results />
         </nav>
     );
 }
---
+++ src/components/nav/results/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Results() {
+    return (
+        <div>
+            <p className="num-results">
+                Found <strong>X</strong> results
+            </p>
+        </div>
+    );
+}
+
+export default Results;
