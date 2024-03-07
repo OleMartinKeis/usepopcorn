e1b4fffa74b9a196b290292f1c481ea60d7ad546
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-03-07 07:52:02 +0100

    Added hover on stars

--- src/components/main/rating/index.jsx
+++ src/components/main/rating/index.jsx
@@ -17,11 +17,12 @@ const textStyle = {
 };
 
 function StarRating({ maxRating = 5 }) {
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
     function handleRating(rating) {
         setRating(rating);
     }
 
-    const [rating, setRating] = useState(0);
     return (
         <div style={containerStyle}>
             <div style={starContainerStyle}>
@@ -29,11 +30,15 @@ function StarRating({ maxRating = 5 }) {
                     <Star
                         key={i}
                         onRate={() => handleRating(i + 1)}
-                        full={rating >= i + 1}
+                        full={
+                            tempRating ? tempRating >= i + 1 : rating >= i + 1
+                        }
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setTempRating(0)}
                     />
                 ))}
             </div>
-            <p style={textStyle}>{rating || ""}</p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 }
