82505f3411d938222e40d8e23d7ebc480d8a778f
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-03-11 08:43:42 +0100

    Moved key

--- src/components/main/left/movies/index.jsx
+++ src/components/main/left/movies/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Movies({ movie }) {
     return (
         <div>
-            <li key={movie.imdbID}>
+            <li>
                 <img src={movie.Poster} alt={`${movie.Title} poster`} />
                 <h3>{movie.Title}</h3>
                 <div>
