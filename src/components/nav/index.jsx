187a6c02cd0c2112f95b48e62d2e114175f08ca4
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-03-11 08:43:31 +0100

    Removed unnecessary imports

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Search from "./search";
-import Logo from "./logo";
-import Results from "./results";
 
 function Navbar({ children }) {
     return <nav className="nav-bar">{children}</nav>;
