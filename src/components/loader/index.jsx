724af6937ece3b71e992ab08b7d47c1e871ded0d
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-10 12:46:26 +0200

    Created loading function

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+    return (
+        <div>
+            <p className="loader">Loading...</p>
+        </div>
+    );
+}
+
+export default Loader;
