31ae808eeed5e2542e1cb32ea0d64cb7236d2261
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-12 12:32:28 +0200

    Adds a button which removes the chosen movie

--- src/components/main/selectMovie/index.jsx
+++ src/components/main/selectMovie/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function SelectMovie({ selectedId }) {
-    return <div className="details">{selectedId}</div>;
+function SelectMovie({ selectedId, setSelectedId }) {
+    /* Removes the selected movie */
+    function handleCloseMovie() {
+        setSelectedId(null);
+    }
+
+    return (
+        <div className="details">
+            <button className="btn-back" onClick={handleCloseMovie}></button>
+            {selectedId}
+        </div>
+    );
 }
 
 export default SelectMovie;
