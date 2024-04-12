4e8e681fcd4aac130ed30724e9be618ae9dabf73
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-12 08:41:56 +0200

    Drilling of props

--- src/components/main/left/movieList/index.jsx
+++ src/components/main/left/movieList/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import Movies from "../movies";
 
-function MovieList({ movies }) {
+function MovieList({ movies, setSelectedId }) {
     return (
         <div>
             <ul className="list">
                 {movies?.map((movie) => (
-                    <Movies movie={movie} key={movie.imdbID} />
+                    <Movies
+                        movie={movie}
+                        key={movie.imdbID}
+                        setSelectedId={setSelectedId}
+                    />
                 ))}
             </ul>
         </div>
