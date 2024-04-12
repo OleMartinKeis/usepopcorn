61f509b4f60b1ddb3eb01b7db1cbd53332b351a6
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-12 08:41:17 +0200

    Update App.jsx
    Removed temp data,
    Added selctId state,
    Drilled setSelectId

--- src/App.jsx
+++ src/App.jsx
@@ -12,48 +12,6 @@ import Loader from "./components/loader";
 import ErrorMesage from "./components/error";
 import SelectMovie from "./components/main/selectMovie";
 
-const tempWatchedData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-        runtime: 148,
-        imdbRating: 8.8,
-        userRating: 10,
-    },
-    {
-        imdbID: "tt0088763",
-        Title: "Back to the Future",
-        Year: "1985",
-        Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-        runtime: 116,
-        imdbRating: 8.5,
-        userRating: 9,
-    },
-];
-
-const tempMovieData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt0133093",
-        Title: "The Matrix",
-        Year: "1999",
-        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt6751668",
-        Title: "Parasite",
-        Year: "2019",
-        Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-    },
-];
-
 const KEY = "dfc8db77";
 
 export default function App() {
@@ -62,7 +20,7 @@ export default function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
     const [query, setQuery] = useState("");
-    const [selectedId, setSelectedId] = useState("tt1375666");
+    const [selectedId, setSelectedId] = useState(null);
 
     //Fetches API results by search filtering and takes the response in setMovies to display
     useEffect(
@@ -96,7 +54,7 @@ export default function App() {
             }
 
             /* if less than three characters in the search field, dont search */
-            if (!query.length < 3) {
+            if (query.length < 3) {
                 setMovies([]);
                 setError("");
                 return;
@@ -122,7 +80,12 @@ export default function App() {
                     {isLoading && <Loader />}
 
                     {/*no isLoading and no error then display movies list*/}
-                    {!isLoading && !error && <MovieList movies={movies} />}
+                    {!isLoading && !error && (
+                        <MovieList
+                            movies={movies}
+                            setSelectedId={setSelectedId}
+                        />
+                    )}
 
                     {/* If there is a error, display error message */}
                     {error && <ErrorMesage message={error} />}
