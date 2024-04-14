a25e0b8bc666d49537b5af329beace1bea2e07aa
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-14 10:26:30 +0200

    Update index.jsx
    movie objects and api call to get a single movie based on id

--- src/components/main/selectMovie/index.jsx
+++ src/components/main/selectMovie/index.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SelectMovie({ selectedId, setSelectedId }) {
+    const [movie, setMovie] = useState({});
+    const KEY = "dfc8db77";
+    const {
+        Title: title,
+        Year: year,
+        Poster: poster,
+        Runtime: runtime,
+        imdbRating,
+        Plot: plot,
+        Released: released,
+        Actors: actors,
+        Director: director,
+        Genre: genre,
+    } = movie;
+    console.log(title, year);
+
     /* Removes the selected movie */
     function handleCloseMovie() {
         setSelectedId(null);
     }
+
+    /*API call to get movies based on ID. Runs each time the component renders */
+    useEffect(function () {
+        async function getMovieDetails() {
+            const res = await fetch(
+                `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            );
+            const data = await res.json();
+            setMovie(data);
+        }
+        getMovieDetails();
+    }, []);
 
     return (
         <div className="details">
