b72eddf99cc9aad636c0a91ca14ed45283deff75
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-18 13:28:36 +0200

    Update useLocalStorageState.jsx
    Added initialState in case of no movies in localstorage on startup

--- src/useLocalStorageState.jsx
+++ src/useLocalStorageState.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
     /* This gets the watched array from localstorage or creats an empty one if localstorage is empty */
     const [value, setValue] = useState(function () {
-        const storedValue = JSON.parse(localStorage.getItem(key) || []);
+        const storedValue =
+            JSON.parse(localStorage.getItem(key)) || initialState;
         return storedValue;
     });
 
