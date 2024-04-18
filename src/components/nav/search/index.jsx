a7e867f9ea18add211b5ce784b9ed9d805341449
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-18 09:06:39 +0200

    Update index.jsx
    Made the search bar a lot better to use with key downs. Read comments for further info

--- src/components/nav/search/index.jsx
+++ src/components/nav/search/index.jsx
@@ -4,9 +4,24 @@ function Search({ query, setQuery }) {
     const inputEl = useRef(null);
 
     /*When we launch the app, you already start in the search bar */
-    useEffect(function () {
-        inputEl.current.focus();
-    }, []);
+    useEffect(
+        function () {
+            /*Lets you remove all text in the search field with enter. Also lets you press enter anywhere in the app and take you back to the search field and clears it*/
+            function callback(e) {
+                /*This makes sure you dont delete the text you have already startet on with enter, if your searchbar is in focus*/
+                if (document.activeElement === inputEl.current) return;
+
+                if (e.code === "Enter") {
+                    inputEl.current.focus();
+                    setQuery("");
+                }
+            }
+
+            document.addEventListener("keydown", callback);
+            return () => document.addEventListener("keydown", callback);
+        },
+        [setQuery]
+    );
 
     return (
         <div>
